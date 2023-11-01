b5de69f4127901ac084513e5790ce193394c071b
choizz <tkfka156@gmail.com>
2023-11-02 06:08:29 +0900

    Initial commit

---
+++ src/main/java/me/choizz/chattingmodule/ChattingmoduleApplication.java
@@ -0,0 +1,13 @@
+package me.choizz.chattingmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingmoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/choizz/chattingmodule/ChattingmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choizz.chattingmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
