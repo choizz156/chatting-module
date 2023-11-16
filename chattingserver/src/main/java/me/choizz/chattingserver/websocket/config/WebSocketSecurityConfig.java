47fefd9e22f932d75f954061fd8c120f96fc59ce
choizz <tkfka156@gmail.com>
2023-11-16 16:25:54 +0900

    form: socketsecurity 주석처리

--- chattingserver/src/main/java/me/choizz/chattingserver/websocket/config/WebSocketSecurityConfig.java
+++ chattingserver/src/main/java/me/choizz/chattingserver/websocket/config/WebSocketSecurityConfig.java
@@ -1,27 +1,22 @@
 package me.choizz.chattingserver.websocket.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.security.authorization.AuthorizationManager;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
-import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 @Configuration
-@EnableWebSocketSecurity
+//@EnableWebSocketSecurity
 public class WebSocketSecurityConfig {
 
-    @Bean
-    public AuthorizationManager<Message<?>> messageAuthorizationManager(
-        MessageMatcherDelegatingAuthorizationManager.Builder messages
-    ) {
-        messages
+//    @Bean
+//    public AuthorizationManager<Message<?>> messageAuthorizationManager(
+//        MessageMatcherDelegatingAuthorizationManager.Builder messages
+//    ) {
+//        messages
 //            .nullDestMatcher().authenticated()
 //            .simpSubscribeDestMatchers("/user/queue/errors").permitAll()
-            .simpDestMatchers("/pub/**").hasRole("USER")
-            .simpSubscribeDestMatchers("sub/**").hasRole("USER")
-            .anyMessage().denyAll();
-
-        return messages.build();
-    }
+//            .simpDestMatchers("/pub/**").hasRole("USER")
+//            .simpSubscribeDestMatchers("sub/**").hasRole("USER")
+//            .anyMessage().denyAll();
+//
+//        return messages.build();
+//    }
 }
