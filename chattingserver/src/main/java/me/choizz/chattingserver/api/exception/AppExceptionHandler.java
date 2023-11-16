f659d1fc6a1b5a0ea263589887e6541d8301760b
choizz <tkfka156@gmail.com>
2023-11-16 22:35:34 +0900

    feat: 500에러 처리

--- chattingserver/src/main/java/me/choizz/chattingserver/api/exception/AppExceptionHandler.java
+++ chattingserver/src/main/java/me/choizz/chattingserver/api/exception/AppExceptionHandler.java
@@ -11,15 +11,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class AppExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponseDto<String> exceptionHandler(Exception e) {
+
+        return new ApiResponseDto<>("알 수 없는 오류가 발생했습니다.");
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResponseDto<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         return new ApiResponseDto<>(ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getBindingResult()));
     }
 
-    @ExceptionHandler(BusinessLoginException.class)
+    @ExceptionHandler(BusinessLogicException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResponseDto<ErrorResponse> businessLogicExceptionHandler(BusinessLoginException e) {
+    public ApiResponseDto<ErrorResponse> businessLogicExceptionHandler(BusinessLogicException e) {
         return new ApiResponseDto<>(ErrorResponse.of(e.getMessage()));
     }
+
+
 }
