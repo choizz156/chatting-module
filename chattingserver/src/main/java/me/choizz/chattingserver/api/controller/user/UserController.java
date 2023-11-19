e69001a47b0eb35c9a680e8ad47a1b13a96d2cda
choizz <tkfka156@gmail.com>
2023-11-19 17:44:47 +0900

    feat: logback 사용 로그 추가

--- chattingserver/src/main/java/me/choizz/chattingserver/api/controller/user/UserController.java
+++ chattingserver/src/main/java/me/choizz/chattingserver/api/controller/user/UserController.java
@@ -1,11 +1,15 @@
 package me.choizz.chattingserver.api.controller.user;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import me.choizz.chattingserver.api.ApiResponseDto;
 import me.choizz.chattingserver.api.controller.user.dto.JoinDto;
 import me.choizz.chattingserver.api.service.user.UserService;
+import me.choizz.chattingserver.domain.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,18 +17,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+//    private static final Logger fileLog = LoggerFactory.getLogger("fileLog");
+    private static final Logger jsonLog = LoggerFactory.getLogger("jsonLog");
+
     private final UserService userService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponseDto<String> join(@RequestBody @Valid JoinDto joinDto){
-        var user = userService.join(joinDto);
+    public ApiResponseDto<String> join(@RequestBody @Valid JoinDto joinDto) {
+
+        User user = userService.join(joinDto);
+
+        jsonLog.info("{}, {}",
+            kv("이메일", joinDto.email()),
+            kv("닉네임", joinDto.nickname())
+        );
+
         return new ApiResponseDto<>(user.getEmail());
     }
 
