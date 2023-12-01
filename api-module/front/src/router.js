9399059fbd5fc34d71e2f75a906c262a3fa87e05
choizz <tkfka156@gmail.com>
2023-12-01 20:26:47 +0900

    feat(front): 권한에 따른 페이지 접속 로직 추가

--- api-module/front/src/router.js
+++ api-module/front/src/router.js
@@ -3,6 +3,7 @@ import UserJoin from "@/components/User-Join.vue";
 import UserLogin from "@/components/User-Login.vue";
 import HomePage from "@/components/Home-Page.vue";
 import ChatRoom from "@/components/Chat-Room.vue";
+import axios from "axios";
 
 const routes = [
   {
@@ -15,16 +16,38 @@ const routes = [
   },
   {
     path: "/login",
+    name: "UserLogin",
     component: UserLogin,
   },
   {
     path: "/chat-room",
     component: ChatRoom,
+    meta: {
+      needAuth: true,
+    },
   },
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+const url = "http://localhost:8080";
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.needAuth)) {
+    axios
+      .get(url + "/users/auth")
+      .then((response) => {
+        if (response.data) {
+          next();
+        }
+      })
+      .catch(() => {
+        next({ name: "UserLogin" });
+        alert("로그인이 필요합니다.");
+      });
+  } else {
+    next();
+  }
 });
 
 export default router;
