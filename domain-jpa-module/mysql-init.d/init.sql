7858a1063cb7dc5026d3bec055b1f57f686450b3
choizz <tkfka156@gmail.com>
2023-12-06 19:57:33 +0900

    refactor: mysql root host 제거

--- domain-jpa-module/mysql-init.d/init.sql
+++ domain-jpa-module/mysql-init.d/init.sql
@@ -1,3 +1,5 @@
-CREATE USER 'choizz'@'choizz-api1.kro.kr ' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'choizz-api1.kro.kr';
+CREATE USER 'choizz'@'36.38.67.6' IDENTIFIED BY '1234';
+CREATE USER 'choizz'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'36.38.67.6';
+GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'localhost';
 CREATE DATABASE choizzdb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
