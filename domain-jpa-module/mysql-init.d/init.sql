eb0d078e33b5d703125bc118e94bdcdb9027bb7b
choizz <tkfka156@gmail.com>
2023-12-06 16:53:12 +0900

    refactor: 설정 변경

--- domain-jpa-module/mysql-init.d/init.sql
+++ domain-jpa-module/mysql-init.d/init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'choizz'@'choizz-api1.kro.kr' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'choizz-api1.kro.kr';
+CREATE USER 'choizz'@'%' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'%';
 CREATE DATABASE choizzdb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
