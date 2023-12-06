546c0df7eed977dfad995c407ed840c1965ed6ef
choizz <tkfka156@gmail.com>
2023-12-06 17:20:28 +0900

    refactor(front): 모듈 위치 변경

---
+++ front/src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @model-open="open" v-if="isOpen" class="modal">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true"></span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>{{ message }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">확인</button>
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+      message: "",
+    };
+  },
+  computed: {
+    isOpen() {
+      return this.isOpen;
+    },
+  },
+  methods: {
+    open(message) {
+      this.isOpen = true;
+      this.message = message;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
