14ee7591d470f27ca12bca864d6932c8a11998c1
choizz <tkfka156@gmail.com>
2023-12-06 15:33:29 +0900

    refactor : 레디스 도커 실행 모듈 변경

--- domain-jpa-module/mysql-init.d/init.sql
+++ domain-jpa-module/mysql-init.d/init.sql
@@ -1,5 +1,3 @@
-CREATE USER 'choizz'@'localhost' IDENTIFIED BY '1234';
-CREATE USER 'choizz'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'%';
+CREATE USER 'choizz'@'choizz-api1.kro.kr' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'choizz'@'choizz-api1.kro.kr';
 CREATE DATABASE choizzdb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
