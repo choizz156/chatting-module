7f10003e752e555192b90ed3807c794efd7c6a2c
choizz <tkfka156@gmail.com>
2023-12-22 19:43:21 +0900

    test: RedisExpirationListenerTest 내장 redis로 변경

--- chatting-redis-module/src/test/java/me/choizz/chattingredismodule/RedisExpirationListenerTest.java
+++ chatting-redis-module/src/test/java/me/choizz/chattingredismodule/RedisExpirationListenerTest.java
@@ -3,22 +3,24 @@ package me.choizz.chattingredismodule;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
+import java.time.Duration;
 import me.choizz.chattingredismodule.dto.LoginUser;
-import me.choizz.chattingredismodule.session.LoginUsers;
 import me.choizz.chattingredismodule.listener.RedisExpirationListener;
+import me.choizz.chattingredismodule.session.LoginUsers;
 import me.choizz.chattingredismodule.session.SessionKeyStore;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.DefaultMessage;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RedisExpirationListenerTest {
 
     @Autowired
@@ -27,33 +29,13 @@ class RedisExpirationListenerTest {
     private SessionKeyStore sessionKeyStore;
     @Autowired
     private LoginUsers loginUsers;
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
     private static RedisClient redisClient;
     private static StatefulRedisConnection<String, String> connection;
     private static RedisCommands<String, String> syncCommands;
-
-    @BeforeAll
-    public static void setup() {
-        // Redis 서버 정보 설정
-        RedisURI redisUri = RedisURI.Builder.redis("localhost", 6379).build();
-
-        // RedisClient 생성
-        redisClient = RedisClient.create(redisUri);
-
-        // Redis 연결 생성
-        connection = redisClient.connect();
-
-        // RedisCommands 인스턴스 생성
-        syncCommands = connection.sync();
-    }
-
-    @AfterAll
-    public static void cleanup() {
-        // Redis 연결 종료
-        connection.close();
-
-        // RedisClient 종료
-        redisClient.shutdown();
-    }
 
 
     @Test
@@ -83,7 +65,7 @@ class RedisExpirationListenerTest {
     void test2() throws Exception {
         String email = "email";
         String sessionKey = "sessionKey";
-        syncCommands.set(sessionKey, email);
+        redisTemplate.opsForValue().set(sessionKey,email);
 
         sessionKeyStore.addValue(email, sessionKey);
         LoginUser loginUser =
@@ -96,7 +78,7 @@ class RedisExpirationListenerTest {
 
         loginUsers.addLoginUser(loginUser);
 
-        syncCommands.expire(sessionKey, 2);
+        redisTemplate.opsForValue().getAndExpire(sessionKey, Duration.ofSeconds(2));
 
         Thread.sleep(3000L);
 
