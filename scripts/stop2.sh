22774c227fe3b617a7345860d37faf47a853f2f1
choizz <tkfka156@gmail.com>
2023-12-25 12:01:17 +0900

    feat: 무중단 배포 hooks에 stop2.sh 추가

---
+++ scripts/stop2.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+txtred='\033[1;31m'
+txtlw='\033[1;33m'
+txtpur='\033[1;35m'
+txtgrn='\033[1;32m'
+txtgrey='\033[1;30m'
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+echo -e "${txtpur} 연결 해제된 포트: $IDLE_PORT"
+IDLE_PID=$(lsof -i :${IDLE_PORT} -t)
+echo -e "${txtpur} 해제할 pid: $IDLE_PID"
+if [ -z ${IDLE_PID} ]; then
+  echo -e "${txtgrn} >> 해당 포트에서 구동 중인 애플리케이션이 없습니다."
+else
+  echo -e "${txtgrn} >> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 3
+  echo -e "${txtpur} 구동 종료"
+fi
