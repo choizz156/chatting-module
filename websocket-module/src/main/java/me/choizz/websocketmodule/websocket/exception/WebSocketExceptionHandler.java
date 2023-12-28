6fb4688cb54148d41dad10c4edca49276bdea519
choizz <tkfka156@gmail.com>
2023-12-29 01:28:06 +0900

    feat: WebSocketExceptionHandler webSocketBusinessExceptionHandler 추가

--- websocket-module/src/main/java/me/choizz/websocketmodule/websocket/exception/WebSocketExceptionHandler.java
+++ websocket-module/src/main/java/me/choizz/websocketmodule/websocket/exception/WebSocketExceptionHandler.java
@@ -4,6 +4,7 @@ import com.mongodb.MongoClientException;
 import com.mongodb.MongoException;
 import com.mongodb.MongoTimeoutException;
 import lombok.extern.slf4j.Slf4j;
+import me.choizz.chattingmongomodule.exception.WebSocketBusinessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -28,14 +29,21 @@ public class WebSocketExceptionHandler {
     @ExceptionHandler(MongoException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public void mongoExceptionHandler(MongoException e) {
-        logger.warn("mongodb error => {}" , e.getMessage());
+        logger.warn("mongodb error => {}", e.getMessage());
         ErrorResponse.of("알 수 없는 에러가 발생했습니다.");
     }
 
     @ExceptionHandler(AuthenticationServiceException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public void authenticationServiceExceptionHandler(AuthenticationServiceException e) {
-        logger.warn("authentication error => {}" , e.getMessage());
+        logger.warn("authentication error => {}", e.getMessage());
         ErrorResponse.of(HttpStatus.UNAUTHORIZED, "인증이 되지 않은 요청입니다.");
+    }
+
+    @ExceptionHandler(WebSocketBusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void webSocketBusinessExceptionHandler(WebSocketBusinessException e) {
+        logger.warn("WebSocketBusinessException error -> {}", e.getMessage());
+        ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
