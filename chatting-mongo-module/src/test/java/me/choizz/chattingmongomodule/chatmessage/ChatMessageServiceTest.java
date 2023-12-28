470edc18065488ecf16ac2a3e51e623c27c11b29
choizz <tkfka156@gmail.com>
2023-12-29 01:24:41 +0900

    test: ChatMessageServiceTest 관련 테스트

--- chatting-mongo-module/src/test/java/me/choizz/chattingmongomodule/chatmessage/ChatMessageServiceTest.java
+++ chatting-mongo-module/src/test/java/me/choizz/chattingmongomodule/chatmessage/ChatMessageServiceTest.java
@@ -1,9 +1,17 @@
 package me.choizz.chattingmongomodule.chatmessage;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.tuple;
 
+import java.util.List;
+import java.util.Optional;
 import me.choizz.chattingmongomodule.chatRoom.ChatRoom;
 import me.choizz.chattingmongomodule.chatRoom.ChatRoomRepository;
+import me.choizz.chattingmongomodule.exception.WebSocketBusinessException;
+import me.choizz.chattingmongomodule.exception.WebSocketExceptionCode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +43,108 @@ class ChatMessageServiceTest {
     @Autowired
     ChatRoomRepository chatRoomRepository;
 
+
+    @BeforeEach
+    void setUpEach() {
+        chatMessageService.checkExistChatRoom(1L);
+    }
+
+    @AfterEach
+    void tearDown() {
+        chatRoomRepository.deleteById(1L);
+    }
+
     @DisplayName("chatRoom 도큐먼트가 존재하지 않는다면 생성한다.")
     @Test
     void test1() throws Exception {
-        chatMessageService.checkExistChatRoom1(1L);
-
+        //give//when
         ChatRoom result = chatRoomRepository.findById(1L).orElseGet(ChatRoom::new);
+
+        //then
         assertThat(result.getRoomId()).isEqualTo(1L);
+    }
+
+    @DisplayName("chatroom 도큐먼트가 존재한다면 메시지만 저장한다.")
+    @Test
+    void test2() throws Exception {
+        //given
+        ChatMessage chatMessage1 = getChatMessage1();
+        ChatMessage chatMessage2 = getChatMessage2();
+
+        //when
+        chatMessageService.checkExistChatRoom(1L);
+        chatMessageService.saveMassage(1L, chatMessage1);
+        chatMessageService.saveMassage(1L, chatMessage2);
+
+        //then
+        Optional<ChatRoom> noRoom = chatRoomRepository.findById(2L);
+        Optional<ChatRoom> chatRoom1 = chatRoomRepository.findById(1L);
+        List<ChatRoom> chatRooms = chatRoomRepository.findAll();
+
+        assertThat(noRoom).isEmpty();
+        assertThat(chatRooms).hasSize(1);
+        assertThat(chatRoom1.get().getRoomId()).isEqualTo(1L);
+        assertThat(chatRoom1.get().getMessageList()).hasSize(2);
+        assertThat(chatRoom1.get().getMessageList())
+            .extracting(
+                "senderId", "receiverId", "senderNickname", "receiverNickname", "roomId", "content"
+            )
+            .containsAnyOf(
+                tuple(2L, 1L, "bbbb", "aaa", 1L, "testest"),
+                tuple(2L, 1L, "aaa", "bbbb", 1L, "testest11111")
+            );
+    }
+
+    @DisplayName("메시지를 조회할 수 있다.")
+    @Test
+    void test3() throws Exception {
+        //given
+        ChatMessage message = getChatMessage1();
+        chatMessageService.saveMassage(1L, message);
+
+        //when
+        List<ChatMessage> chatMessages = chatMessageService.findChatMessages(1L);
+
+        //then
+        assertThat(chatMessages).hasSize(1);
+        assertThat(chatMessages)
+            .extracting(
+                "senderId", "receiverId", "senderNickname", "receiverNickname", "roomId", "content"
+            )
+            .contains(tuple(2L, 1L, "bbbb", "aaa", 1L, "testest"));
+    }
+
+    @DisplayName("메시지 조회시 채팅방이 없다면 예외를 던진다.")
+    @Test
+    void test4() throws Exception {
+        //given//when
+        ChatMessage message = getChatMessage1();
+        chatMessageService.saveMassage(1L, message);
+        //then
+        assertThatThrownBy(() -> chatMessageService.findChatMessages(2L))
+            .isInstanceOf(WebSocketBusinessException.class)
+            .hasMessageContaining(WebSocketExceptionCode.NO_CHAT_ROOM.getMsg());
+    }
+
+    private ChatMessage getChatMessage1() {
+        return ChatMessage.builder()
+            .roomId(1L)
+            .content("testest")
+            .receiverNickname("aaa")
+            .senderId(2L)
+            .senderNickname("bbbb")
+            .receiverId(1L)
+            .build();
+    }
+
+    private ChatMessage getChatMessage2() {
+        return ChatMessage.builder()
+            .roomId(1L)
+            .content("testest11111")
+            .receiverNickname("bbbb")
+            .senderId(1L)
+            .senderNickname("aaa")
+            .receiverId(2L)
+            .build();
     }
 }
