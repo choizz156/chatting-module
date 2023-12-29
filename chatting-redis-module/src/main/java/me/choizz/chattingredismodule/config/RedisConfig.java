5bddfc95734f894b530b1d811f83c39d206cace3
choizz <tkfka156@gmail.com>
2023-12-29 19:01:43 +0900

    fix: RedisConfig 적용 프로파일 수정

--- chatting-redis-module/src/main/java/me/choizz/chattingredismodule/config/RedisConfig.java
+++ chatting-redis-module/src/main/java/me/choizz/chattingredismodule/config/RedisConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Profile("*prod*")
+@Profile("!test")
 @Configuration
 public class RedisConfig {
 
